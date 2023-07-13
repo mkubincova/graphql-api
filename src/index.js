86cf560bf4e788432148c7cebd1d5b0630bd4127
mkubincova <mkubincova@proton.me>
2023-07-13 14:08:47 +0200

    feat: mutations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { v4 as uuid } from 'uuid';
 
 // data
 import db from '../_db.js';
@@ -25,6 +26,26 @@ const resolvers = {
     Review: {
         author: (parent) => db.authors.find((author) => author.id === parent.author_id),
         game: (parent) => db.games.find((game) => game.id === parent.game_id)
+    },
+    Mutation: {
+        deleteGame: (_, args) => db.games = db.games.filter((game) => game.id !== args.id),
+        addGame: (_, args) => {
+            let game = {
+                ...args.game,
+                id: uuid()
+            };
+            db.games.push(game);
+            return game;
+        },
+        updateGame: (_, args) => {
+            db.games = db.games.map((game) => {
+                if (game.id === args.id) {
+                    return { ...game, ...args.edits };
+                }
+                return game;
+            });
+            return db.games.find((game) => game.id === args.id);
+        }
     }
 };
 
@@ -38,4 +59,4 @@ const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
 });
 
-console.log('Server ready at port', 4000);
+console.log(`Server ready at ${url}`);
--- src/schema.js
+++ src/schema.js
@@ -26,4 +26,17 @@ export const typeDefs = `#graphql
         authors: [Author]
         author(id: ID!): Author
     }
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+        addGame(game: AddGameInput!): Game
+        updateGame(id: ID!, edits: UpdateGameInput!): Game
+    }
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
+    input UpdateGameInput {
+        title: String
+        platform: [String!]
+    }
 `;
