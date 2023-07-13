7da321a8d8c59f5232ffa40609aa9ea764130e7f
mkubincova <mkubincova@proton.me>
2023-07-13 12:55:08 +0200

    feat: query variables

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ import { typeDefs } from './schema.js';
 const resolvers = {
     Query: {
         reviews: () => db.reviews,
+        review: (_, args) => db.reviews.find((review) => review.id === args.id),
         games: () => db.games,
+        game: (_, args) => db.games.find((game) => game.id === args.id),
         authors: () => db.authors,
+        author: (_, args) => db.authors.find((author) => author.id === args.id),
     },
 };
 
